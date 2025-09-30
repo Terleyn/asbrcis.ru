4a06984b3eee38669de0ff9bb74bc5960fdc850c
Terleyn <alex.onoprienko2912@gmail.com>
2025-09-30 14:29:42 +0300

    Добавляем для гугла штуки

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,43 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'ASBR:CIS',
-  description: 'Официальный сайт ASBR:CIS',
+  description: 'Русское сообщество по JoJo’s Bizarre Adventure: All-Star Battle R',
   generator: 'ASBR:CIS',
+  keywords: [
+    'ASBR',
+    'JoJo',
+    'JoJo’s Bizarre Adventure',
+    'All-Star Battle R',
+    'ASBR CIS',
+    'Русское сообщество JoJo',
+    'JoJo Discord',
+    'JoJo Fighting Game'
+  ],
+  authors: [{ name: 'ASBR:CIS Team' }],
+  metadataBase: new URL('https://asbrcis.ru'),
+
+  openGraph: {
+    title: 'ASBR:CIS',
+    description: 'Русское сообщество по JoJo’s Bizarre Adventure: All-Star Battle R',
+    url: 'https://asbrcis.ru',
+    siteName: 'ASBR:CIS',
+    images: [
+      {
+        url: '/server-logo.png',
+        width: 1200,
+        height: 630,
+        alt: 'ASBR:CIS Logo',
+      },
+    ],
+    locale: 'ru_RU',
+    type: 'website',
+  },
+
+  icons: {
+    icon: '/favicon.ico', 
+    shortcut: '/favicon.ico',
+    apple: '/server-logo.png',
+  },
 }
 
 export default function RootLayout({
@@ -16,7 +51,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="ru">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
         <Analytics />
